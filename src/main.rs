c45007eed673950c1ed6db7de22ff0deb47d6b10
Linara Sankubaeva <linarasankubaeva55@mail.ru>
2025-10-25 22:18:11 +0300

    Made a program that takes positive numbers from stdin until it meets -1 and outputs their sum to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
+use std::io;
 
+fn main() {
+	let mut sum = 0;
+
+	loop {
+		let mut input = String::new();
+		io::stdin()
+			.read_line(&mut input)
+			.expect("Failed input");
+		let input = input.trim();
+
+		if input == "-1" {
+			break;
+		}
+
+		match input.parse::<i32>() {
+			Ok(number) => {
+				if number >= 0 {
+					sum += number;
+				} else {
+					println!("NaN");
+					return;
+				}
+			},
+			Err(_) => {
+				println!("NaN");
+				return;
+			},
+		}
+	}
+
+	println!("{}", sum);
 }
